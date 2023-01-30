4ddfaddae33f1ce9658824a388266bf8fd01145a
Daniels Hogans <Z309372@CLR0ZN001232523>
2023-01-30 08:29:12 -0300

    partes novas

--- active-Jquery.js
+++ active-Jquery.js
@@ -4,7 +4,18 @@ $(document).ready(function(){
     $('a[href]').on('click', function(e){
         e.preventDefault();
         $('div[id^=P_]').hide();
-        $('#P_'+e.originalEvent.srcElement.attributes.href.value).show();
+        let url = e.originalEvent.srcElement.attributes.href.value;
+        $('#P_'+url).show();
+        window.pushState = url;
+        location.href.replace("/(.*?)/", url);
+            console.log(url);
+        //console.log(window.history);
+        //window.location.href = url;
+        //window.history.replaceState(null,null, url);
+        //history.pushState(null, null);
+        //window.history.pushState({}, null ,url);
+        
+
         //window.location.href.value = e.originalEvent.srcElement.attributes.href.value;
         //$('#'+window.location.href.value).hide();
         
